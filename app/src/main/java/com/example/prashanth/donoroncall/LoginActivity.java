b87275b09be9fdaec136c49be2ed9d1ec16025c8
prashanth9962 <prashanth.engr@gmail.com>
2015-12-11 16:43:15 +0530

    skeleton map fragment

--- app/src/main/java/com/example/prashanth/donoroncall/LoginActivity.java
+++ app/src/main/java/com/example/prashanth/donoroncall/LoginActivity.java
@@ -7,6 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -15,15 +21,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        GoogleMap googleMap;
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        final LatLng TutorialsPoint = new LatLng(21 , 57);
+        Marker TP = googleMap.addMarker(new MarkerOptions().position(TutorialsPoint).title("TestLocation"));
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
