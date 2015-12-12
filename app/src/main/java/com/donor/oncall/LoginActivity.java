3644230bfd8b531d9fa7c9e9b4575b3c74e381c3
prashanth9962 <prashanth.engr@gmail.com>
2015-12-12 10:30:03 +0530

    skeleton commit for map fragment

--- app/src/main/java/com/donor/oncall/LoginActivity.java
+++ app/src/main/java/com/donor/oncall/LoginActivity.java
@@ -3,11 +3,6 @@ package com.donor.oncall;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,10 +12,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        GoogleMap googleMap;
-        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        final LatLng TutorialsPoint = new LatLng(21 , 57);
-        Marker TP = googleMap.addMarker(new MarkerOptions().position(TutorialsPoint).title("TestLocation"));
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/donor/oncall/MainActivity.java
+++ app/src/main/java/com/donor/oncall/MainActivity.java
@@ -6,15 +6,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    static final LatLng IITChennai = new LatLng(12.991992, 80.237090);
+    static final LatLng TidelPark = new LatLng(12.989504, 80.248420);
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if (map!=null){
+            // Move the camera instantly to hamburg with a zoom of 15.
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(IITChennai, 15));
+
+            // Zoom in, animating the camera.
+            map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+            Marker hamburg = map.addMarker(new MarkerOptions().position(IITChennai)
+                    .title("IITChennai"));
+            Marker kiel = map.addMarker(new MarkerOptions()
+                    .position(TidelPark)
+                    .title("TidelPark")
+                    .snippet("TidelPark is cool"));
+        }
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
