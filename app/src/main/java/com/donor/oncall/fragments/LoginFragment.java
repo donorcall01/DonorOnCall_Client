d2a6288aaee72abdd198897b84080041c809b677
prashanth9962 <prashanth.engr@gmail.com>
2016-01-17 15:19:10 +0530

    validations for login screen

--- app/src/main/java/com/donor/oncall/fragments/LoginFragment.java
+++ app/src/main/java/com/donor/oncall/fragments/LoginFragment.java
@@ -6,55 +6,125 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.donor.oncall.DonorApi.LoginApi;
 import com.donor.oncall.DonorApi.ServiceGenerator;
 import com.donor.oncall.R;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.json.JSONObject;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+import retrofit.mime.TypedByteArray;
 
 /**
  * Created by prashanth on 3/1/16.
  */
 public class LoginFragment extends BaseFragment {
-    String TAG = "LoginActivity";
+    private static String TAG = "LoginActivity";
+    private static String errorMess="The Username or Password is wrong";
+    private static EditText passwordField,usernameField;
+    private static String password,username;
+    private static  View rootView=null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.login_screen, container, false);
+        rootView = inflater.inflate(R.layout.login_screen, container, false);
+        setupValueField();
+        setUpLogin();
+        return rootView;
+
+        //return rootView;
+    }
+
+    public void setupValueField(){
+        usernameField = (EditText) rootView.findViewById(R.id.usrname);
+        passwordField = (EditText) rootView.findViewById(R.id.pwd);
+    }
+
+    public  boolean validateEmailId(String email){
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        boolean status = false;
+        if (email == null || email.isEmpty()){
+            usernameField.setError("User name cannot be empty");
+        }else {
+            if (email.matches(emailPattern)){
+               status = true;
+            }else {
+                usernameField.setError("Not a valid email Address");
+            }
+        }
+     return status;
+    }
+
+    public  boolean validatePassword(String password){
+        //return password != null || !password.isEmpty();
+        boolean status = true;
+        if (password == null || password.isEmpty()){
+            passwordField.setError("Password cannot be empty");
+            status = false;
+        }
+        return status;
+    }
+
+    public boolean  validateEmailIdAndPassword(){
+        username = usernameField.getText().toString();
+        password =passwordField.getText().toString();
+        boolean status = false;
+        if (validateEmailId(username) && validatePassword(password))
+            status = true;
+        return status;
+    }
+    public  void setUpLogin(){
         final LoginApi loginApi = ServiceGenerator.createService(LoginApi.class);
         rootView.findViewById(R.id.signin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 JSONObject jsonObject = new JSONObject();
-                try {
-                    jsonObject.put("userName","pontiyaraja@gmail.com");
-                    jsonObject.put("passWord","Pan");
-                }catch (Exception e){
+                if (validateEmailIdAndPassword()) {
+                    try {
 
+                        jsonObject.put("userName", username);
+                        jsonObject.put("passWord", password);
+
+
+                        loginApi.login(jsonObject, new Callback<Response>() {
+                            @Override
+                            public void success(Response response, Response response2) {
+                                Log.d(TAG, "Success " + response.getReason());
+                                String json = new String(((TypedByteArray) response.getBody()).getBytes());
+                                JsonParser parser = new JsonParser();
+                                JsonElement responseJson = parser.parse(json);
+                                signUpResponse(responseJson.getAsJsonObject());
+                                Log.d(TAG, "Success " + json);
+                            }
+
+                            @Override
+                            public void failure(RetrofitError error) {
+                                Log.d(TAG, "Success " + error.getMessage());
+                            }
+                        });
+
+
+                    } catch (Exception e) {
+
+                    }
                 }
-
-                loginApi.login(jsonObject, new Callback<Response>() {
-                    @Override
-                    public void success(Response response, Response response2) {
-                        Log.d(TAG, "Success " + response.getReason());
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-                        Log.d(TAG, "Success " + error.getMessage());
-                        Log.d(TAG, "url " + error.getUrl());
-                        //Log.d(TAG, "Body " + error.getBody().toString());
-                        Log.d(TAG, "Error " + error.toString() );
-                    }
-                });
             }
         });
-        return rootView;
-        //return rootView;
+    }
+
+    public void signUpResponse(JsonObject jsonObject){
+        if (jsonObject.get("success").getAsBoolean()){
+
+        }else {
+            Toast.makeText(getActivity(),errorMess,Toast.LENGTH_LONG).show();
+        }
     }
 }
