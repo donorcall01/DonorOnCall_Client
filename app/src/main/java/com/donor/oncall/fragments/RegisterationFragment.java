548005ad63da4c2a7bf697c6f14bc5327de00da9
prashanth9962 <prashanth.engr@gmail.com>
2016-01-17 19:04:03 +0530

    do validations and api call

--- app/src/main/java/com/donor/oncall/fragments/RegisterationFragment.java
+++ app/src/main/java/com/donor/oncall/fragments/RegisterationFragment.java
@@ -4,20 +4,33 @@ import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.donor.oncall.DonorApi.LoginApi;
+import com.donor.oncall.DonorApi.ServiceGenerator;
 import com.donor.oncall.R;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.mime.TypedByteArray;
 
 /**
  * Created by prashanth on 3/1/16.
@@ -26,7 +39,10 @@ public class RegisterationFragment extends BaseFragment {
 
     private static  View rootView=null;
     private static EditText usrnameField,pwdField,cfrmpwdField,nameField,dobField;
+    private static CheckBox tos,donor,recipient;
     private static Spinner bloodGroupField;
+    private static String TAG ="RegisterationFragment";
+    private static String password,username,cfrmpassword,name,dob,bloodGrp,type;
     Calendar calendar = Calendar.getInstance();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,7 +56,7 @@ public class RegisterationFragment extends BaseFragment {
         setupFields();
         setUpDropDown();
         setupDateTimeField();
-        setUpValidationForFields();
+        setUpRegisteration();
     }
 
     public void setupFields(){
@@ -50,6 +66,8 @@ public class RegisterationFragment extends BaseFragment {
       nameField = (EditText) rootView.findViewById(R.id.name);
       dobField = (EditText) rootView.findViewById(R.id.dob);
       bloodGroupField = (Spinner) rootView.findViewById(R.id.bloodgrp);
+      donor = (CheckBox) rootView.findViewById(R.id.donor);
+      recipient = (CheckBox) rootView.findViewById(R.id.recipient);
     }
 
     public void setUpDropDown(){
@@ -77,20 +95,159 @@ public class RegisterationFragment extends BaseFragment {
         dobField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                new DatePickerDialog(getActivity(), date, calendar
-                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+                if (hasFocus) {
+                    new DatePickerDialog(getActivity(), date, calendar
+                            .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+
             }
         });
     }
 
     private void updateDob() {
-        String dateFormat = "DD/MM/yy"; //In which you need put here
+        String dateFormat = "yyyy-MM-DD"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
         dobField.setText(sdf.format(calendar.getTime()));
     }
 
-    public void setUpValidationForFields(){
+    public  boolean isValidEmail(String value){
+        boolean status = true;
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (isNullOrEmpty(value)){
+            if (!value.matches(emailPattern)){
+                usrnameField.setError("Enter a valid Email-id");
+                status = false;
+            }
+        }else{
+            usrnameField.setError("User name cannot be empty");
+            status = false;
+        }
+        return status;
+        }
 
+
+    public boolean isNullOrEmpty(String value){
+        boolean status = true;
+        if (value == null || value.isEmpty())
+            status =false;
+        return  status;
+    }
+
+    public boolean checkPwd(){
+        boolean status = true;
+        if (isNullOrEmpty(password)){
+            if (isNullOrEmpty(cfrmpassword)){
+                  if (!password.equals(cfrmpassword)){
+                      cfrmpwdField.setError("Passwords donot match");
+                      status = false;
+                  }
+            }else {
+                cfrmpwdField.setError("Confirm password cannot be empty");
+                status=false;
+            }
+
+        }else {
+            pwdField.setError("Password cannot be empty");
+            status=false;
+        }
+        return status;
+    }
+
+
+    public boolean checkName(){
+        boolean status = true;
+        if (!isNullOrEmpty(name)){
+            nameField.setError("Name  cannot be empty");
+            status = false;
+        }
+        return status;
+    }
+
+    public boolean checkDob(){
+        boolean status = true;
+        if (!isNullOrEmpty(dob)){
+            dobField.setError("DOB  cannot be empty");
+            status = false;
+        }
+        return status;
+    }
+
+    public boolean checkDonorRecipient(){
+        boolean status = true;
+        if (donor.isChecked()){
+            type = "Donor";
+        }else {
+            donor.setError("Select either donor or recipient");
+        }
+        if (recipient.isChecked()){
+            type = "Recipient";
+        }
+
+        return status;
+    }
+
+    public boolean validateFields(){
+        boolean status = true;
+
+        if (isValidEmail(username)
+                && checkPwd()
+                && checkName()
+                && checkDob()
+                && checkDonorRecipient()) {
+
+        }else {
+            status = false;
+        }
+        return status;
+    }
+
+    public void setFieldValues(){
+        username=usrnameField.getText().toString();
+        password=pwdField.getText().toString();
+        cfrmpassword=cfrmpwdField.getText().toString();
+        dob=dobField.getText().toString();
+        name=nameField.getText().toString();
+        bloodGrp =bloodGroupField.getSelectedItem().toString();
+    }
+
+    public void setUpRegisteration(){
+        final LoginApi loginApi = ServiceGenerator.createService(LoginApi.class);
+       rootView.findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               setFieldValues();
+               JSONObject jsonObject = new JSONObject();
+               if (validateFields()){
+                 try {
+
+                     jsonObject.put("userName", username);
+                     jsonObject.put("passWord", password);
+                     jsonObject.put("name", name);
+                     jsonObject.put("bloodGroup", bloodGrp);
+                     jsonObject.put("type", type);
+                     jsonObject.put("dob", dob);
+                     loginApi.register(jsonObject,new Callback< Response >() {
+                         @Override
+                         public void success(Response response, Response response2) {
+                             //Log.d(TAG, "Success " + response.getReason());
+                             String json = new String(((TypedByteArray) response.getBody()).getBytes());
+                             JsonParser parser = new JsonParser();
+                             JsonElement responseJson = parser.parse(json);
+                             //signUpResponse(responseJson.getAsJsonObject());
+                             Log.d(TAG, "Success " + json);
+                         }
+
+                         @Override
+                         public void failure(RetrofitError error) {
+                             Log.d(TAG, "Success " + error.getMessage());
+                         }
+                     });
+                 }catch (Exception e){
+
+                 }
+               }
+           }
+       });
     }
 }
