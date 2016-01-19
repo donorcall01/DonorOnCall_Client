bc4aadd1abdf37a472fe9fedffff1712b00eedf3
prashanth9962 <prashanth.engr@gmail.com>
2016-01-19 13:25:16 +0530

    get location details

--- app/src/main/java/com/donor/oncall/MainActivity.java
+++ app/src/main/java/com/donor/oncall/MainActivity.java
@@ -1,5 +1,6 @@
 package com.donor.oncall;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -25,20 +26,28 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         setUpToolBar();
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-
+        map.setMyLocationEnabled(true);
         if (map!=null){
+            map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+                @Override
+                public void onMyLocationChange(Location arg0) {
+                    map.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
+                    map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+                }
+            });
             // Move the camera instantly to hamburg with a zoom of 15.
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(IITChennai, 15));
+           // map.moveCamera(CameraUpdateFactory.newLatLngZoom(IITChennai, 15));
 
             // Zoom in, animating the camera.
-            map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+            //map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
 
-            Marker hamburg = map.addMarker(new MarkerOptions().position(IITChennai)
+           /* Marker hamburg = map.addMarker(new MarkerOptions().position(IITChennai)
                     .title("IITChennai"));
             Marker kiel = map.addMarker(new MarkerOptions()
                     .position(TidelPark)
                     .title("TidelPark")
-                    .snippet("TidelPark is cool"));
+                    .snippet("TidelPark is cool"));*/
         }
 
     }
