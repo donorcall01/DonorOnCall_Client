c38a37433185e92c6b586c1f712bc9fda600e5a9
prashanth9962 <prashanth.engr@gmail.com>
2016-01-30 18:44:13 +0530

    setup dropdown fiels

--- app/src/main/java/com/donor/oncall/fragments/RequestBloodFragment.java
+++ app/src/main/java/com/donor/oncall/fragments/RequestBloodFragment.java
@@ -5,7 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.donor.oncall.R;
 
@@ -14,8 +16,9 @@ import com.donor.oncall.R;
  */
 public class RequestBloodFragment extends BaseFragment {
 
-    private static  View rootView=null;
-    private static EditText hospitalField,patientField,purposeField,unitsField,howsoonField;
+    private  View rootView=null;
+    private  EditText hospitalField,patientField,purposeField,unitsField,howsoonField;
+    private  Spinner bloodGroupField;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.request_blood, container, false);
@@ -28,7 +31,15 @@ public class RequestBloodFragment extends BaseFragment {
         //strict ordering
         setUpViews();
         setUpRequestButton();
+        setUpDropDown();
 
+    }
+
+    public void setUpDropDown(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.blood_group, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bloodGroupField.setAdapter(adapter);
     }
 
     public void setUpViews(){
@@ -37,6 +48,7 @@ public class RequestBloodFragment extends BaseFragment {
         purposeField = (EditText) rootView.findViewById(R.id.purpose);
         unitsField = (EditText) rootView.findViewById(R.id.units);
         howsoonField = (EditText) rootView.findViewById(R.id.howsoon);
+        bloodGroupField = (Spinner) rootView.findViewById(R.id.bloodgrp);
     }
 
     public void setUpRequestButton(){
