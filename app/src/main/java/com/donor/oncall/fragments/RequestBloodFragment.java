7332cb30d2a68d4087651ebe72d053449b0f2ce4
prashanth9962 <prashanth.engr@gmail.com>
2016-01-30 18:42:18 +0530

    assign and check for fields

--- app/src/main/java/com/donor/oncall/fragments/RequestBloodFragment.java
+++ app/src/main/java/com/donor/oncall/fragments/RequestBloodFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.donor.oncall.R;
 
@@ -14,6 +15,7 @@ import com.donor.oncall.R;
 public class RequestBloodFragment extends BaseFragment {
 
     private static  View rootView=null;
+    private static EditText hospitalField,patientField,purposeField,unitsField,howsoonField;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.request_blood, container, false);
@@ -24,6 +26,25 @@ public class RequestBloodFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //strict ordering
+        setUpViews();
+        setUpRequestButton();
 
+    }
+
+    public void setUpViews(){
+        hospitalField = (EditText) rootView.findViewById(R.id.hospital);
+        patientField = (EditText) rootView.findViewById(R.id.patient);
+        purposeField = (EditText) rootView.findViewById(R.id.purpose);
+        unitsField = (EditText) rootView.findViewById(R.id.units);
+        howsoonField = (EditText) rootView.findViewById(R.id.howsoon);
+    }
+
+    public void setUpRequestButton(){
+        rootView.findViewById(R.id.requestBlood).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
