c108dc4baf5528724154030985c6333deb0557fc
prashanth9962 <prashanth.engr@gmail.com>
2016-04-26 11:30:53 +0530

    add production domain

--- app/src/main/java/com/donor/oncall/DonorApi/ServiceGenerator.java
+++ app/src/main/java/com/donor/oncall/DonorApi/ServiceGenerator.java
@@ -10,11 +10,11 @@ import retrofit.client.OkClient;
  */
 public class ServiceGenerator {
 
-    public static final String production_domain = "http://ec2-52-74-63-14.ap-southeast-1.compute.amazonaws.com";
+    public static final String production_domain = "http://54.169.220.144";
     public static  final String debug_domain = "http://192.168.0.103:8000";
 
     private static RestAdapter.Builder builder = new RestAdapter.Builder()
-            .setEndpoint(debug_domain)
+            .setEndpoint(production_domain)
             .setClient(new OkClient(new OkHttpClient()));
 
     public static <S> S createService(Class<S> serviceClass) {
